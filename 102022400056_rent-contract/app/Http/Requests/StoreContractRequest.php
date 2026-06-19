1513c901665222962028e0f06d2351c55eca8322
EsGoreng <itsnaakhdan25@gmail.com>
2026-06-19 21:26:41 +0700

    fix request resource

---
+++ 102022400056_rent-contract/app/Http/Requests/StoreContractRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreContractRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+        {
+            return [
+                'tenant_id'  => ['required', 'int'],
+                'listing_id' => ['required', 'int',],
+                'start_date' => ['required', 'date'],
+                'end_date'   => ['required', 'date', 'after:start_date'],
+                'is_active'  => ['sometimes'],
+            ];
+        }
+}
---
+++ 102022400056_rent-contract/app/Http/Requests/StoreTenantRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTenantRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255', 'unique:tenants,email,'.($this->tenant->id ?? 'NULL')],
+            'phone' => ['required', 'string', 'max:20'],
+            'nik' => ['required', 'string', 'max:20', 'unique:tenants,nik,'.($this->tenant->id ?? 'NULL')],
+        ];
+    }
+}

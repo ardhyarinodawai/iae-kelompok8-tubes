872fa00f83542e798cb843d0cb0e18c41a235123
Rafsanzani28 <muhammadrafsanzani382@gmail.com>
2026-06-19 18:24:51 +0700

    update user.php Models

---
+++ 102022400128_service-listing-unit/app/Models/User.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class User extends Model
+{
+    protected $fillable = [
+        'email',
+        'name',
+    ];
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function hasRole(string $role): bool
+    {
+        return $this->roles()->where('name', $role)->exists();
+    }
+}
